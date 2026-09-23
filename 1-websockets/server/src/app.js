c9b4eb06097f4f2a638369af600c303ccc126bee
Ayan-Siddique <26sidayan@gmail.com>
2026-09-23 11:48:03 +0530

    Refactor WebSocket message handling and improve code formatting

--- 1-websockets/server/src/app.js
+++ 1-websockets/server/src/app.js
@@ -17,13 +17,15 @@ wss.on("connection", (socket) => {
   socket.on("message", (data, isBinary) => {
     console.log(data.toString());
     wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data ,{binary: isBinary});
-    }
-  });
-  });
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+    socket.on("close", () => {
+      console.log("Client disconnected");
+    });
 
-  
-
-  socket.send("connect established to Web-socket");
+    console.log("users", ++user);
+    
+  });
 });
